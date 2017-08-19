1a0905f5e9189800cc6c9a8dc9cb9ec2fc193098
Aaron Arinder <aaronarinder@gmail.com>
2017-08-19 18:46:26 -0400

    adding payment interface

--- Server/client/src/actions/index.js
+++ Server/client/src/actions/index.js
@@ -8,3 +8,12 @@ export const fetchUser = () => {
       .then(res => dispatch({ type: FETCH_USER, payload: res.data }));
   };
 };
+
+//posts token to backend
+export const handleToken = token => {
+  return function(dispatch) {
+    axios
+      .post('/api/stripe', token)
+      .then(res => dispatch({ type: FETCH_USER, payload: res.data }));
+  };
+};
