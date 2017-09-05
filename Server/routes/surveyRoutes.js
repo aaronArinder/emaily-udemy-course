d978fde2ec99b9c899d95a4bfb36743043b4ebd7
Aaron Arinder <aaronarinder@gmail.com>
2017-09-05 14:20:57 -0400

    initial path-parser setup for collecting response data

--- Server/routes/surveyRoutes.js
+++ Server/routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const Path = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -9,6 +12,28 @@ const Survey = mongoose.model('surveys');
 module.exports = app => {
   app.get('/api/surveys/thanks', (req, res) => {
     res.send('Thanks for voting!');
+  });
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const p = new Path('/surveys/:surveyId/:choice');
+
+    const events = _.map(req.body, ({ email, url }) => {
+      const match = p.test(new URL(url).pathname);
+      if (match) {
+        return { email, surveyId: match.surveyId, choice: match.choice };
+      }
+    });
+
+    //tests the path for a match, taking out :surveyId and :choice and returning
+    //an object
+
+    const compactEvents = _.compact(events);
+    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+    //removes duplicates
+    console.log(uniqueEvents);
+    res.send({});
+    //to stop sendgrid from resending the post request (without response, they
+    // think it failed, resulting in tons of console.logs)
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
